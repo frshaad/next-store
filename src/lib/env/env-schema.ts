4d90543574c7ce4fd1c4b13fc583465fee4ec201
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-07-10 13:53:35 +0330

    Update auth environment variables schema and config

--- src/lib/env/env-schema.ts
+++ src/lib/env/env-schema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod'
 
 export const environmentSchema = z.object({
+  AUTH_SECRET: z.string(),
+  AUTH_GOOGLE_ID: z.string(),
+  AUTH_GOOGLE_SECRET: z.string(),
+  AUTH_GITHUB_ID: z.string(),
+  AUTH_GITHUB_SECRET: z.string(),
   DATABASE_URL: z.string(),
 })
 
