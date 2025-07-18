817dd8e488183fba3a8d28f654a398e86f310ffb
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-07-18 17:12:12 +0330

    Add upvotes and comments relations to post schema

--- src/db/schema/post.ts
+++ src/db/schema/post.ts
@@ -1,5 +1,7 @@
 import { relations } from 'drizzle-orm'
 import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { comments } from './comment'
+import { postUpvotes } from './upvote'
 import { users } from './user'
 
 const posts = pgTable('posts', {
@@ -17,11 +19,13 @@ const posts = pgTable('posts', {
   updatedAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
 })
 
-const postsRelations = relations(posts, ({ one }) => ({
+const postRelations = relations(posts, ({ one, many }) => ({
   author: one(users, {
     fields: [posts.userId],
     references: [users.id],
   }),
+  comments: many(comments),
+  postUpvotes: many(postUpvotes),
 }))
 
-export { posts, postsRelations }
+export { posts, postRelations }
